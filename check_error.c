0d11daa7080f2686808d91210e5f30d61255d04b
AugustusTonia <gtonia983@gmail.com>
2022-12-17 12:00:16 -0800

    c files

---
+++ check_error.c
@@ -0,0 +1,141 @@
+#include "monty.h"
+
+/**
+ * is_string_printable - check that
+ * @str: str to check
+ *
+ * Return: 1 if printable
+ */
+int is_string_printable(char *str)
+{
+	int i = 0;
+	int is_print = 0;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (isprint(str[i]))
+		{
+			is_print = 1;
+			break;
+		}
+		i++;
+	}
+
+	return (is_print);
+}
+
+/**
+ * check_args - check arg length
+ * @argc: arg to check it's length
+ *
+ * Return: void
+ */
+void check_args(int argc)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * get_file - get wanted file
+ * @path: path to file
+ *
+ * Return: FILE
+ */
+FILE *get_file(const char *path)
+{
+	FILE *f = fopen(path, "r");
+
+	if (f == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", path);
+		exit(EXIT_FAILURE);
+	}
+
+	return (f);
+}
+
+/**
+ * check_op_code - checkt the op code
+ * @str: op_code
+ * @line_number: line number
+ */
+void check_op_code(char *str, int line_number)
+{
+	char *op_codes[] = {"pall", "pall\n",
+						"push",
+						"pint", "pint\n",
+						"pop", "pop\n",
+						"swap", "swap\n",
+						"add", "add\n",
+						"sub", "sub\n",
+						"nop", "nop\n",
+						"pchar", "pchar\n",
+						"mod", "mod\n",
+						"div", "div\n",
+						"mul", "mul\n",
+						"pstr", "pstr\n",
+						"rotl", "rotl\n",
+						"rotr", "rotr\n",
+						"queue", "queue\n",
+						"stack", "stack\n",
+						NULL};
+
+	int i = 0;
+
+	while (op_codes[i] != NULL)
+	{
+		if (strcmp(op_codes[i], str) == 0)
+			return;
+		i++;
+	}
+	if (str[strlen(str) - 1] == '\n')
+		str[strlen(str) - 1] = '\0';
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, str);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * check_arg2 - checks arg 2
+ * @arg_2: arg 2
+ *
+ * Return: int
+ */
+int check_arg2(char *arg_2)
+{
+	int a = 0, i = 0;
+
+	if (arg_2 == NULL)
+		return (INT_NULL);
+
+	a = atoi(arg_2);
+	i = strlen(arg_2);
+
+	if (arg_2[0] == '-' && i == 1)
+	{
+		return (INT_NULL);
+	}
+
+	i = 0;
+	while (arg_2[i] != 0 && isprint(arg_2[i]))
+	{
+		if (i == 0 && arg_2[i] == '-')
+		{
+			i++;
+			continue;
+		}
+		else
+		{
+			if (!isdigit(arg_2[i]))
+				return (INT_NULL);
+		}
+		i++;
+	}
+
+	return (a);
+}
---
+++ linkedlist3.c
@@ -0,0 +1,189 @@
+#include "monty.h"
+
+/**
+ * mul - sutracts top of stack
+ * @h: head node
+ * @ln: line number
+ *
+ * Return: number of nodes
+ */
+size_t mul(stack_t *h, int ln)
+{
+	size_t number_of_nodes = 0;
+	stack_t *temp = h;
+	int temp_num = 0;
+
+	if (h == NULL || h->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", ln);
+		exit(EXIT_FAILURE);
+	}
+
+	while (temp && temp->next != NULL)
+	{
+		number_of_nodes++;
+		temp = temp->next;
+	}
+
+	while (temp != NULL)
+	{
+		temp_num = abs(temp->prev->n * temp->n);
+		delend_dnodeint(&head);
+		if (head && head->next == NULL)
+		{
+			free(head);
+			head = NULL;
+		}
+		else
+			delend_dnodeint(&head);
+		add_dnodeint_end(&head, temp_num);
+		break;
+	}
+	return (number_of_nodes);
+}
+
+/**
+ * mod - sutracts top of stack
+ * @h: head node
+ * @ln: line number
+ *
+ * Return: number of nodes
+ */
+size_t mod(stack_t *h, int ln)
+{
+	size_t number_of_nodes = 0;
+	stack_t *temp = head;
+	int temp_num = 0;
+
+	if (h == NULL || h->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", ln);
+		exit(EXIT_FAILURE);
+	}
+
+	while (temp && temp->next != NULL)
+	{
+		number_of_nodes++;
+		temp = temp->next;
+	}
+
+	while (temp != NULL)
+	{
+		if (temp->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", ln);
+			exit(EXIT_FAILURE);
+		}
+
+		temp_num = abs(temp->prev->n % temp->n);
+		delend_dnodeint(&head);
+		if (head && head->next == NULL)
+		{
+			free(head);
+			head = NULL;
+		}
+		else
+			delend_dnodeint(&head);
+		add_dnodeint_end(&head, temp_num);
+		break;
+	}
+	return (number_of_nodes);
+}
+
+/**
+ * peek_char - print top of stack
+ * @h: head node
+ * @ln: line number
+ *
+ * Return: number of nodes
+ */
+size_t peek_char(stack_t *h, int ln)
+{
+	size_t number_of_nodes = 0;
+	stack_t *temp = h;
+
+	if (h == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", ln);
+		exit(EXIT_FAILURE);
+	}
+	while (temp && temp->next != NULL)
+	{
+		number_of_nodes++;
+		temp = temp->next;
+	}
+	while (temp != NULL)
+	{
+		if (temp->n >= 0 && temp->n <= 127)
+			printf("%c\n", temp->n);
+		else
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", ln);
+			exit(EXIT_FAILURE);
+		}
+		break;
+	}
+	return (number_of_nodes);
+}
+
+/**
+ * printstr_dlistintrev - print chars
+ * @h: head node
+ *
+ * Return: number of nodes
+ */
+size_t printstr_dlistintrev(stack_t *h)
+{
+	size_t number_of_nodes = 0;
+	stack_t *temp = h;
+
+	if (h == NULL)
+	{
+		printf("\n");
+		return (number_of_nodes);
+	}
+
+	while (temp && temp->next != NULL)
+	{
+		number_of_nodes++;
+		temp = temp->next;
+	}
+
+	while (temp != NULL)
+	{
+		if (temp->n >= 1 && temp->n <= 127)
+			printf("%c", temp->n);
+		else
+		{
+			printf("\n");
+			return (number_of_nodes);
+		}
+		temp = temp->prev;
+	}
+	printf("\n");
+	return (number_of_nodes);
+}
+
+/**
+ * rotate_left - top of stack becomes first
+ * @h: head
+ *
+ * Return: void
+ */
+void rotate_left(stack_t *h)
+{
+	stack_t *temp = h;
+	int hold_last_n = 0;
+
+	if (h == NULL || (h && h->next == NULL))
+		return;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+
+	hold_last_n = temp->n;
+	delend_dnodeint(&head);
+	add_dnodeint(&head, hold_last_n);
+}
